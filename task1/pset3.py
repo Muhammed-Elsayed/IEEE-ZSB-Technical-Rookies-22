9ccdadf8141eb5161ec61e98179a88e021107ce6
mo6502 <70416898+mo6502@users.noreply.github.com>
2021-12-17 19:53:01 +0200

    Create pset3.py

---
+++ task1/pset3.py
@@ -0,0 +1,22 @@
+#python
+list1 = [1, 2, 3, 4, 5, 6, 7]
+
+
+def binary_search(list, wanted_number):
+    low = 0
+    high = len(list1) - 1
+
+    while True:
+        mid = (low + high) // 2
+
+        if wanted_number == list1[mid]:
+            return mid
+
+        elif wanted_number > list1[mid]:
+            low = mid + 1
+
+        elif wanted_number < list1[mid]:
+            high = mid - 1
+
+
+print(binary_search(list1, 5))
