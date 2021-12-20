5e11ad52b30a906df0ce6727064d1983ade112c4
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2021-12-20 14:46:40 +0200

    Update pset1.py

--- task2/pset1.py
+++ task2/pset1.py
@@ -1,1 +1,10 @@
+#python
+def is_a_palindrome(word):
+    if word == word[::-1]:
+       return True
 
+    else:
+        return False
+
+word_given = input(" please enter a word : ")
+print(is_a_palindrome(word_given))
