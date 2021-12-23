0fba3d29b630c4e7e4d6533ff71f7733965dc46d
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2021-12-23 23:57:01 +0200

    Update pset5.py

--- task3/pset5.py
+++ task3/pset5.py
@@ -1,3 +1,6 @@
+length_of_list = int(input("enter the length of the list: "))
+scores = list(map(int, input("enter the elements of the list here: ").split()))
+
 def breakingRecords(scores):
     min_count = 0
     max_count = 0
@@ -6,18 +9,15 @@ def breakingRecords(scores):
 
 
     for n in scores:
-        print("mo")
         if n < lowest_score:
             lowest_score = n
             min_count += 1
-            print(min_count)
+
         elif n > highest_score:
-            
             highest_score = n
-
             max_count += 1
-            print(max_count)
 
+    return [max_count, min_count]
 
-    list1 = [max_count, min_count]
-    return list1
+print(breakingRecords(scores))
+
