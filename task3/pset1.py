69b7ce920435e5934abc5b84a835ffbad126f9f4
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2021-12-23 20:18:09 +0200

    Update pset1.py

--- task3/pset1.py
+++ task3/pset1.py
@@ -1,1 +1,17 @@
+def water_bottles():
+    n = int(input("please enter a positive number : "))
+    Total_remaining_volume = 0
+    list1 = []
+    for index in range(n):
+        remaining_volume, capacity = map(int, input("enter two numbers: ").split())
+        Total_remaining_volume += remaining_volume
+        list1.append(capacity)
 
+    list1.sort(reverse = True)
+
+    if Total_remaining_volume > list1[0] + list1[1]:
+        return ("no")
+    else:
+        return ("yes")
+
+print(water_bottles())
