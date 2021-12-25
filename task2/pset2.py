1f3a34bbaf85f0e8fd96d96fbba400ef3ae6addc
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2021-12-25 23:39:06 +0200

    Update pset2.py

--- task2/pset2.py
+++ task2/pset2.py
@@ -1,16 +1,14 @@
 #python
 
 # getting the list and sort it first
-mylist = list(map(int, input("please enter the list: ").split()))
+mylist = list(map(int, input("please enter the list: ").split(" ")))
 mylist.sort()
 
-# removing duplicates by using dictionary
+
 def remove_duplicates(list):
-    dict1 = {}
     new_list = []
     for index in list:
-      if index not in dict1:
-          dict1[index] = 1
+      if index not in new_list:
           new_list.append(index)
 
     print(*new_list)
