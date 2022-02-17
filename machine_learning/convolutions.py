a54588c05f87211a73d3a286228bb218f555b081
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-02-17 03:32:28 +0200

    Create convolutions.py

---
+++ machine_learning/convolutions.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten,Conv2D, Maxpooling2D
+import pickle
+
+x = pickle.load(open("x.pickle","rb"))
+y = pickle.load(open("y.pickle","rb"))
+
+x = x / 255.0
+
+model = Sequential()
+model.add(Conv2D(64), (3, 3), input_shape = x.shape[1:])
+model.add(Activation("relu"))
+model.add(Maxpooling2D(pool_size = (2,2)))
+
+model.add(Conv2D(64), (3, 3))
+model.add(Activation("relu"))
+model.add(Maxpooling2D(pool_size = (2,2)))
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+model.compile(loss = "binary_crossentropy", optimizer = "adam", metrics = ['accuracy'])
+model.fit(x, y, batch_size = 32, validation_split = 0.1)
